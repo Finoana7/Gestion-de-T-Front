e14e5ef1d6125c97865a1ed2e3cca42eaf97df4c
Fabrich Vohanson <128395240+fabrichgit@users.noreply.github.com>
2024-08-19 11:16:21 +0300

    Update index.ts

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,3 +1,3 @@
-export const api = 'http://localhost:3000'
+export const api = 'https://juma-api.onrender.com'
 export const token = () => `Bearer ${sessionStorage.getItem('token')}`
 export const cloud = `https://api.cloudinary.com/v1_1/dtoe2dj7i/image/upload`
